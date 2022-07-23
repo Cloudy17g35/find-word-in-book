38766be5b973f73568ec1df745f1c57261f07787
Cloudy17g35 <ksawery.lejczak@gmail.com>
2022-07-23 13:43:14 +0200

    adding error handling for external requests

--- find_word_in_book/external_requests.py
+++ find_word_in_book/external_requests.py
@@ -1,19 +1,26 @@
 import requests
-
+from fastapi import HTTPException
 
 
 def get_request(url: str):
     try:
         response:requests.Response = requests.get(url)
         return response
-    except requests.exceptions.Timeout:
-        # change to custom error
-        raise ValueError()
+    except Exception as e:
+        print(e)
+        message: str = "dependency error"
+        raise HTTPException(status_code=424,
+                            detail=message
+                            )
+        
 
 
 def abort_if_response_not_ok(response:requests.Response):
     if not response.ok:
-        raise ValueError("invalid response")
+        message = f"invalid status code:{response.status_code} from external site"
+        raise HTTPException(status_code=424,
+                            detail=message
+                            )
 
 
 def get_book_text(url:str, encoding:str) -> str:
